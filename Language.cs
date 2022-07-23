efca7edf3bd988c0a8bd4d42a28e3bfb733a768b
jontcont <52921053+JontCont@users.noreply.github.com>
2022-07-23 19:46:02 +0800

    測試 Language

--- Language.cs
+++ Language.cs
@@ -10,14 +10,14 @@ namespace MES_WORK
 
         public Language()
         {
-            string lanString = "zh-TW";
+            string lanString ="en-US" ;//"zh-TW";
             //取得或設定 CultureInfo 物件，表示目前線程和以工作為基礎的非同步作業所使用的文化特性。
             //目前線程和以工作為基礎的非同步作業所使用的文化特性。
-            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(lanString);
+            //System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(lanString);
             //取得或設定 CultureInfo 物件，此物件代表 Resource Manager 用於執行階段查詢特定文化特性資源的目前使用者介面文化特性。
             //資源管理員用來在執行階段查詢特定文化特性資源的文化特性。
-            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lanString);
-            
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lanString);
+
         }
         public string GetName(string str)
         {
